d8212c2feeac979c5bbeba924156abbc80e31667
hello-world-teste <fabio.schonrock@gmail.com>
2021-06-10 19:52:38 -0300

    Atividade 4 questao 3 commit

--- Atividade04/src/PalavraDeAcesso.java
+++ Atividade04/src/PalavraDeAcesso.java
@@ -1,17 +1,22 @@
 import javax.swing.JOptionPane;
 
 public class PalavraDeAcesso {
+
 	public static void main(String[] args) {
-		String p1 = JOptionPane.showInputDialog("Por favor digite a palavra passe!");
-		String p2 = "Java-2021";
-		if (!p1.equals(p2)) {
-			for (int i = 2; i > 1 && i <= 2; i--) {
+			String passe;
+			String senha = "Java-2021";
+			int tent = 2;
+			passe = JOptionPane.showInputDialog("Por favor digite a palavra passe!");
+			if (passe.equals(senha)) {
+				JOptionPane.showMessageDialog(null, "Acesso liberado!");
+			} else
+				do {
 				JOptionPane.showMessageDialog(null,
-						"Acesso negado! Tente novamente. Você tem apenas + " + i + " tentativas");
-			}
-		} else {
-			JOptionPane.showMessageDialog(null, "Acesso permitido!");
+						"Palavra incorreta! Por favor digite novamente.\n Restam apenas " + tent + " tentativas!");
+				passe = JOptionPane.showInputDialog("Por favor digite a palavra passe!");
+				tent--;
+			} while (!senha.equals(passe) || tent >= 0);
+
 		}
 
 	}
-}
